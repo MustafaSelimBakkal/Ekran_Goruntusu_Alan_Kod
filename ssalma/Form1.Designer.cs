edf976e7c9fe867235caec66fe48ce47fa0cb4d8
Mustafa Selim Bakkal <130986941+MustafaSelimBakkal@users.noreply.github.com>
2023-04-20 22:46:24 +0300

    Add files via upload

---
+++ ssalma/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ssalma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(86, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SS AL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 51);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Home Tuşu ss alır";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ssalma/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ssalma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap Screenshot()
+        {
+            // Ekran görüntüsü alırken formu kapatır ve böylece ekranda gözükmez
+            this.Opacity = 0;
+
+            // ss alan kodumuz
+            Bitmap Screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics GFX = Graphics.FromImage(Screenshot);
+            GFX.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+
+            // formu geri getiren kod
+            this.Opacity = 1;
+
+            return Screenshot;
+        }
+
+        string KayitYolu = @"C:\Ekran_Resimleri";
+        string ResimAdi()
+        {
+            return "SS_" + DateTime.Now.ToString().Replace(" ", "_").Replace(":", "_") + ".jpg";
+            // Örnek Resim Adı : SS_20.04.2023_12_48_50.jpg 
+            // Saniyeye kadar aldıkki resim adları çakışmasın. Çakışma olması durumda eskiyi siler. Üzerine yazar.
+        }
+
+        void Kaydet(string resimAdi)
+        {
+            // C yerel diskinde bir klasör açıyoruz. 
+            // Klasör varsa hiçbir işlem yapmayacak
+            // Klasör yoksa klasör oluşturacak.
+            if(!File.Exists(KayitYolu))
+                Directory.CreateDirectory(KayitYolu);
+            // Aldığımız resmi kayıt ediyoruz.
+            Screenshot().Save(KayitYolu + "\\" + resimAdi + "", ImageFormat.Jpeg);
+
+        }
+
+        //Home tuşu jısa yol olucak kısayolun kodu
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Home)
+                Kaydet(ResimAdi());
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kaydet(ResimAdi());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
